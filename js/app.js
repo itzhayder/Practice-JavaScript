ef82ca39e5c5168ff8155124916e34308cb91d0c
itzhayder <mdhayder.ali@northsouth.edu>
2019-07-08 02:40:32 +0600

    javascript Object learning

--- js/app.js
+++ js/app.js
@@ -1,86 +1,130 @@
 
-// array.forEach(callback(currentValue [, index [, array]])[, thisArg])
-// array.filter(element, index, array)
-// array.find()
-// array.every()
+// // array.forEach(callback(currentValue [, index [, array]])[, thisArg])
+// // array.filter(element, index, array)
+// // array.find()
+// // array.every()
 
-const inventory = [
-    {name: "Banana", quantity: 4},
-    {name: "Apple", quantity: 2},
-    {name: "Jack Fruit", quantity: 1}
-];
+// const inventory = [
+//     {name: "Banana", quantity: 4},
+//     {name: "Apple", quantity: 2},
+//     {name: "Jack Fruit", quantity: 1}
+// ];
 
-const person = [
-    {name: "Hayder", role: "Student"},
-    {name: "Nafisa", role: "Student"},
-    {name: "Atiqur", role:"VC"},
-    {name: "Adnan", role: "Teacher"},
-    {name: "SMU1", role: "Teacher"}
-];
+// const person = [
+//     {name: "Hayder", role: "Student"},
+//     {name: "Nafisa", role: "Student"},
+//     {name: "Atiqur", role:"VC"},
+//     {name: "Adnan", role: "Teacher"},
+//     {name: "SMU1", role: "Teacher"}
+// ];
 
-function getUser(userType) {
-    const users = person.filter( el => el.role === userType);
-    return users;
-}
+// function getUser(userType) {
+//     const users = person.filter( el => el.role === userType);
+//     return users;
+// }
 
-const students = getUser("Teacher");
-console.log(students);
+// const students = getUser("Teacher");
+// console.log(students);
 
-//  Promise Practice
+// //  Promise Practice
 
-const count = 0;
-const promise1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if (count) {
-            resolve({
-                name: "Hayder",
-                id: "1521231642",
-                department: "ECE",
-                major: "CSE"
-            });
-        }
-        else {
-            reject("I broke the promise.");
-        }
-    }, 2000);
-});
+// const count = 0;
+// const promise1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (count) {
+//             resolve({
+//                 name: "Hayder",
+//                 id: "1521231642",
+//                 department: "ECE",
+//                 major: "CSE"
+//             });
+//         }
+//         else {
+//             reject("I broke the promise.");
+//         }
+//     }, 2000);
+// });
 
-promise1
-.then(res => {
-    console.log(res);
-})
-.catch(err => {
-    console.log(err);
-})
+// promise1
+// .then(res => {
+//     console.log(res);
+// })
+// .catch(err => {
+//     console.log(err);
+// })
 
-console.log(promise1);
-async function prom() {
-    try {
-        const result = await promise1;
-        console.log(result);
+// console.log(promise1);
+// async function prom() {
+//     try {
+//         const result = await promise1;
+//         console.log(result);
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// prom();
+
+
+// const doctor = name => {
+//     console.log(name);
+// }
+
+// doctor("Dr. Tousif Rahman");
+
+// const hero = ['Lina', 'Ursa', 'Witch Doctor', 'Lina', 'Phantom Assassin'];
+// const take = 'Ursa';
+
+// const selected = hero.filter(el => el === 'Lina');
+
+// console.log(selected);
+
+// // ==== Local and Session Storage ====
+
+// localStorage.setItem('name', 'Hayder');
+// localStorage.clear();
+
+
+// ==== Object ====
+
+const cat = {
+    age: 2,
+    name: 'Bailey',
+    meow: function () {
+        console.log('Meow!');
+    },
+    greet: function (name) {
+        console.log(`Hello ${name}`);
     }
-    catch (err) {
-        console.log(err);
-    }
 }
 
-prom();
+cat.age++;
 
+const printer = {};
 
-const doctor = name => {
-    console.log(name);
+printer.on = true;
+printer.mode = 'black and white';
+printer.print = function () {
+    console.log('The printer is printing');
 }
 
-doctor("Dr. Tousif Rahman");
+delete printer.mode;
 
-const hero = ['Lina', 'Ursa', 'Witch Doctor', 'Lina', 'Phantom Assassin'];
-const take = 'Ursa';
+function changeToEight (n) {
+    n = 8;
+}
+let n = 7;
+changeToEight(n);
 
-const selected = hero.filter(el => el === 'Lina');
+let originalObject = {
+    favoriteColor:'red'
+}
 
-console.log(selected);
+function setToBlue (object) {
+    object.favoriteColor = 'blue';
+}
 
-// ==== Local and Session Storage ====
+setToBlue(originalObject);
 
-localStorage.setItem('name', 'Hayder');
-localStorage.clear();
+
